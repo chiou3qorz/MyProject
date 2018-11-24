447b5fa3682ca2d3199ef9adea02973d7cb22f09
chiou3qorz <chiou@e-happy.com.tw>
2018-11-24 09:46:18 +0800

    update commit

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name = 'MyProject',
+    packages = ['mypackage'],
+    scripts = [],
+    version = '1.0',
+    description = 'My first project',
+    author = 'chiou',
+    author_email = 'chiou@e-happy.com.tw',
+    url = 'https://github.com/chiou3qorz/MyProject.git',
+    download_url = 'https://github.com/chiou3qorz/MyProject/tarball/v1.0',
+    keywords = ['Good Project'],
+    classifiers = [],
+)
